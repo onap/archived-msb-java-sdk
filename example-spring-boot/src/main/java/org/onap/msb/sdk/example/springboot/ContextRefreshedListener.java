3d5fe8914883143fe34c15e80f9510383fc6478b
virajput <vijendra_rajput@infosys.com>
2017-09-01 00:49:58 +0530

    Added license header and other review comments
    Issue-Id: MSB-23
    Change-Id: I58ac54a53d8fb281c6284139ba5323732f3ec773
    Signed-off-by: virajput <vijendra_rajput@infosys.com>

---
+++ example-spring-boot/src/main/java/org/onap/msb/sdk/example/springboot/ContextRefreshedListener.java
@@ -0,0 +1,40 @@
+/*******************************************************************************
+ * Copyright 2017 ZTE, Inc. and others.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ ******************************************************************************/
+package org.onap.msb.sdk.example.springboot;
+
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+import org.onap.msb.sdk.httpclient.msb.MSBServiceClient;
+import org.onap.msb.sdk.example.springboot.common.MsbHelper;
+
+@Component
+public class ContextRefreshedListener implements ApplicationListener<ContextRefreshedEvent>{
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
+        System.out.println("Registring Service...");
+        String MSB_IP="127.0.0.1";
+        int MSB_Port=10081;
+
+        MSBServiceClient msbClient = new MSBServiceClient(MSB_IP, MSB_Port);
+        MsbHelper helper = new MsbHelper(msbClient);
+        
+        try {
+			helper.registerMsb();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+    }
+}
---
+++ example-spring-boot/src/main/java/org/onap/msb/sdk/example/springboot/common/MsbHelper.java
@@ -0,0 +1,52 @@
+/*******************************************************************************
+ * Copyright 2017 ZTE, Inc. and others.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ ******************************************************************************/
+package org.onap.msb.sdk.example.springboot.common;
+
+import java.net.InetAddress;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.onap.msb.sdk.discovery.entity.MicroServiceInfo;
+import org.onap.msb.sdk.discovery.entity.Node;
+import org.onap.msb.sdk.httpclient.msb.MSBServiceClient;
+
+public class MsbHelper {
+
+  private MSBServiceClient msbClient;
+
+  public MsbHelper(MSBServiceClient msbClient) {
+    super();
+    this.msbClient = msbClient;
+  }
+
+  public void registerMsb() throws Exception {
+
+
+    MicroServiceInfo msinfo = new MicroServiceInfo();
+
+    msinfo.setServiceName("employee");
+    msinfo.setVersion("v1");
+    msinfo.setUrl("/api/v1");
+    msinfo.setProtocol("REST");
+    msinfo.setVisualRange("0|1");
+    
+    Set<Node> nodes = new HashSet<>();
+    Node node1 = new Node();
+    node1.setIp(InetAddress.getLocalHost().getHostAddress());
+    node1.setPort("8080");
+    nodes.add(node1);
+    msinfo.setNodes(nodes);
+    msbClient.registerMicroServiceInfo(msinfo, false);
+  }
+}
