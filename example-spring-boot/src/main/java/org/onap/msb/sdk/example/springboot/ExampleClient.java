2ddd3c62d3fb0f45a1ef2ae161b3a65aa06ae716
virajput <vijendra_rajput@infosys.com>
2017-09-01 21:43:34 +0530

    added spring boot example using msb java sdk
    Issue-Id: MSB-23
    Change-Id: I789dc5f101a3e511f9f4ccc6f023c57489ea28f6
    Signed-off-by: virajput <vijendra_rajput@infosys.com>

--- example-spring-boot/src/main/java/org/onap/msb/sdk/example/springboot/ExampleClient.java
+++ example-spring-boot/src/main/java/org/onap/msb/sdk/example/springboot/ExampleClient.java
@@ -1,9 +1,11 @@
 /*******************************************************************************
  * Copyright 2017 Infosys Limited and others.
- *------------------------------------------------------------------------------
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
@@ -17,7 +19,6 @@ import org.onap.msb.sdk.example.springboot.model.Employee;
 import org.onap.msb.sdk.httpclient.RestServiceCreater;
 import org.onap.msb.sdk.httpclient.msb.MSBServiceClient;
 
-
 public class ExampleClient {
 
   /**
@@ -26,7 +27,7 @@ public class ExampleClient {
    */
   public static void main(String[] args) throws IOException {
     //For real use case, MSB IP and Port should come from configuration file instead of hard code here
-    String MSB_IP="192.168.0.110";
+    String MSB_IP="127.0.0.1";
     int MSB_Port=10081;
 
     MSBServiceClient msbClient = new MSBServiceClient(MSB_IP, MSB_Port);
