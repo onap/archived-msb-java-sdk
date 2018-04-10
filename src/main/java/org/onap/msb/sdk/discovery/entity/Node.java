c1f429787220f9e9682251d1ca794d442bcac4bb
LiZi <li.zi30@zte.com.cn>
2018-04-10 04:20:36 -0400

    Remove the default value of check time set
    Change-Id: I55aaeb39f95c1e82d9661f983ceb00b8e2e19868
    Issue-ID: MSB-189
    Signed-off-by: LiZi <li.zi30@zte.com.cn>

--- src/main/java/org/onap/msb/sdk/discovery/entity/Node.java
+++ src/main/java/org/onap/msb/sdk/discovery/entity/Node.java
@@ -36,10 +36,10 @@ public class Node implements Serializable {
     private String checkUrl = "";
 
     // TCP or HTTP health check Interval,Unit: second", example = "10s"
-    private String checkInterval = "10s";
+    private String checkInterval;
 
     // TCP or HTTP health check TimeOut,Unit: second", example = "10s"
-    private String checkTimeOut = "10s";
+    private String checkTimeOut;
 
 
     public String getIp() {
