63bc01d53baa0b0262daf7f5a1201e8ba23a66b5
Chandan Ghosh <cghosh12@in.ibm.com>
2018-10-04 11:11:24 +0530

    Added junit testcase for JacksonRequestBodyConvert
    Added junit testcase for JacksonRequestBodyConverter
    
    Issue-ID: MSB-292
    Change-Id: If8711f35b0bfa28fbfa8d91fcc312601ac391c49
    Signed-off-by: Chandan Ghosh <cghosh12@in.ibm.com>

---
+++ src/test/java/org/onap/msb/sdk/httpclient/convert/jackson/JacksonRequestBodyConverterTest.java
@@ -0,0 +1,64 @@
+/*
+ * ============LICENSE_START==========================================
+ * ONAP Portal SDK
+ * ===================================================================
+ * Copyright © 2017 IBM Intellectual Property. All rights reserved.
+ * ===================================================================
+ *
+ * Unless otherwise specified, all software contained herein is licensed
+ * under the Apache License, Version 2.0 (the "License");
+ * you may not use this software except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *             http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Unless otherwise specified, all documentation contained herein is licensed
+ * under the Creative Commons License, Attribution 4.0 Intl. (the "License");
+ * you may not use this documentation except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *             https://creativecommons.org/licenses/by/4.0/
+ *
+ * Unless required by applicable law or agreed to in writing, documentation
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * ============LICENSE_END============================================
+ *
+ * 
+ */
+package org.onap.msb.sdk.httpclient.convert.jackson;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+public class JacksonRequestBodyConverterTest {
+
+	@Test
+	public void convertTest() throws IOException {
+		ObjectMapper mapper = new ObjectMapper();
+	    mapper.configure(SerializationFeature.WRAP_ROOT_VALUE, false);
+	    ObjectWriter ow = mapper.writer().withDefaultPrettyPrinter();
+		JacksonRequestBodyConverter jrbc=new JacksonRequestBodyConverter(ow);
+		List<String> sampleValues=new ArrayList<>();
+		sampleValues.add("one");
+		sampleValues.add("two");
+		Assert.assertNotNull(jrbc.convert(sampleValues));
+	}
+}
